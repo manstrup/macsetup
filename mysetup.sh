91f2fe1733b9f147c289b69f1cff715687a4f9ef
Thomas Manstrup Knudsen <tmkdesigner@gmail.com>
2025-11-04 09:12:45 +0100

    Add files via upload

---
+++ mysetup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Opdater Homebrew
+brew update
+
+# Installer programmer
+brew install wget
+brew install git
+brew install node
+brew install ffmpeg
+
+# Installer GUI apps via Homebrew Cask
+brew install --cask imageoptim
+brew install --cask appcleaner
+brew install --cask visual-studio-code
+brew install --cask firefox
+brew install --cask vlc
+brew install --cask spotify
+brew install --cask sonos
+brew install --cask discord
+brew install --cask whatsapp
+brew install --cask messenger
+brew install --cask dropbox
+
+# Ryd op
+brew cleanup
