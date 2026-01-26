9901c9980c144186df7773edd410d3ed580a160e
Thomas Manstrup Knudsen <tmkdesigner@gmail.com>
2026-01-26 12:29:31 +0100

    Replace Visual Studio Code with Zed in setup script

--- mysetup.sh
+++ mysetup.sh
@@ -12,16 +12,14 @@ brew install ffmpeg
 # Installer GUI apps via Homebrew Cask
 brew install --cask imageoptim
 brew install --cask appcleaner
-brew install --cask visual-studio-code
+brew install --cask zed
 brew install --cask firefox
 brew install --cask vlc
 brew install --cask spotify
 brew install --cask sonos
 brew install --cask discord
 brew install --cask whatsapp
-brew install --cask messenger
 brew install --cask dropbox
-brew install --cask ghostty
 
 # Ryd op
 brew cleanup
